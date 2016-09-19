c8db867fa457a5604910095600f0079d64596c8d
bthi <bthi@truste.com>
2016-09-18 17:37:41 -0700

    initial commit

---
+++ src/main/java/com/brianthi/DiscoverSpApplication.java
@@ -0,0 +1,12 @@
+package com.brianthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverSpApplication.class, args);
+	}
+}
